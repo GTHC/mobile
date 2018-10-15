b4a2cdd975496c42117eb0fe0bf9175d94267598
Anesu Mafuvadze <mafuvadzeanesu@gmail.com>
2018-10-14 23:20:31 -0400

    Get google auth working on android Why: So we can login to firebase
    This change addresses the need by:
    Setting up android side changes to be able to login with google auth
    
    Test plan:
    - click login
    - see google login prompt

--- Home.js
+++ Home.js
@@ -1,12 +1,15 @@
 // @flow
 
-import { createBottomTabNavigator } from 'react-navigation';
-import { Personal, Group, Settings, Availability } from './HomeTabs';
+import { createBottomTabNavigator } from 'react-navigation'; // eslint-disable-line no-unused-vars
+import { Personal, Group, Settings, Availability } from './HomeTabs'; // eslint-disable-line no-unused-vars
+import Login from './Auth/Login';
+
+export default Login;
 
 // TODO(anesu): Add icons for the tabs
-export default createBottomTabNavigator({
-  Personal,
-  Group,
-  Settings,
-  Availability,
-});
+// export default createBottomTabNavigator({
+//   Personal,
+//   Group,
+//   Settings,
+//   Availability,
+// });
