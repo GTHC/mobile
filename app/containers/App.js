e9474c6528b7d9feab71e7fd88882b28dbc87e09
vinitowinit <vinitparekh24@gmail.com>
2019-12-11 13:08:37 -0500

    Initialized OneSignal SDK wiht EventListnersS

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React, {Component} from 'react';
+import OneSignal from 'react-native-onesignal'; 
 import {Provider} from 'react-redux';
 import {NavigationNativeContainer} from '@react-navigation/native';
 import {configureStore} from '../redux/utils/store';
@@ -11,6 +12,35 @@ import AppNavigator from '../navigation/AppNavigator';
 type Props = any;
 
 export default class App extends Component<Props> {
+  constructor(properties) {
+    super(properties);
+    OneSignal.init("TBD ONE_Signal_ID");
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
   render() {
     const {store} = configureStore();
 
@@ -23,3 +53,4 @@ export default class App extends Component<Props> {
     );
   }
 }
+
