cf16d1dd91374ae332cb62c0ef1a7fa3d74d8edd
Rikki Kendall <yrk3@duke.edu>
2019-12-19 00:39:38 -0500

    Bottom Tab Navigation Icons

--- app/navigation/BottomTabNavigator.js
+++ app/navigation/BottomTabNavigator.js
@@ -5,12 +5,31 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import TeamCalendar from '../containers/TeamCalendar';
 import UpcomingShifts from '../containers/UpcomingShifts';
 import SettingsTabs from '../containers/SettingsTabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Calendar') {
+            iconName = 'ios-calendar';
+          } else if (route.name === 'Settings') {
+            iconName = 'ios-settings';
+          } else if (route.name === 'Shifts') {
+            iconName = 'ios-apps';
+          }
+          return <Ionicons name={iconName} size={25} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}>
       <Tab.Screen name="Calendar" component={TeamCalendar} />
       <Tab.Screen name="Shifts" component={UpcomingShifts} />
       <Tab.Screen name="Settings" component={SettingsTabs} />
