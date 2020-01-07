ecb80112fdac89fd1b3f6bd0278425c69becc5be
Ansh Nanda <anshnanda10@gmail.com>
2020-01-07 13:01:59 -0500

    create two new views-- line monitor and personal shift notifs pages

--- app/containers/Notifications.js
+++ app/containers/Notifications.js
@@ -1,110 +1,41 @@
 // @flow
 
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { StyleSheet, View, FlatList} from 'react-native';
-import { Title } from 'native-base';
-import { getNotifications} from '../redux/actions/notifications';
-import moment from 'moment';
 import {
-  Text,
-  List,
-  ListItem,
+  Button,
+  Icon,
+  Title,
+  Right,
+  Left,
+  Container,
+  Header,
+  Content,
+  Tab,
+  Tabs,
   Body,
 } from 'native-base';
+import UserAnnouncements from '../components/UserAnnouncements';
+import UserNotifications from '../components/UserNotifications';
 
-const NOTIFICATIONS = [{"status":"SUCCESS",
- "message":"Notifications for user with netid ssh50 found.",
- "data":{
- 	"notifications": [
- 		{"id":75,"notification_id":"693b18a6-0703-4511-8bde-798f14db792a","start_time":"2020-01-09T09:20:00.000Z","title":"Upcoming Shift Reminder","content":"Hey! Your tent shift in K-Ville starts in 10 minutes!","user_id":31,"created_at":"2020-01-06T00:45:09.549Z","updated_at":"2020-01-06T00:45:09.549Z"}, {"id":75,"notification_id":"693b18a6-0703-4511-8bde-798f14db792a","start_time":"2020-01-09T09:20:00.000Z","title":"Upcoming Shift Reminder","content":"Hey! Your tent shift in K-Ville starts in 10 minutes!","user_id":31,"created_at":"2020-01-06T00:45:09.549Z","updated_at":"2020-01-06T00:45:09.549Z"}, {"id":75,"notification_id":"693b18a6-0703-4511-8bde-798f14db792a","start_time":"2020-01-09T09:20:00.000Z","title":"Upcoming Shift Reminder","content":"Hey! Your tent shift in K-Ville starts in 10 minutes!","user_id":31,"created_at":"2020-01-06T00:45:09.549Z","updated_at":"2020-01-06T00:45:09.549Z"}],
- 	"announcements": [
- 		{"id":1,"title":"Line Monitor Announcement","body":"This is a line monitor announcement. ","created_at":"2020-01-06T03:44:57.029Z","updated_at":"2020-01-06T03:44:57.029Z"}, {"id":2,"title":"Line Monitor Announcement","body":"This is a line monitor announcement. ","created_at":"2020-01-06T03:44:57.029Z","updated_at":"2020-01-06T03:44:57.029Z"}, {"id":3,"title":"Line Monitor Announcement","body":"This is a line monitor announcement. ","created_at":"2020-01-06T03:44:57.029Z","updated_at":"2020-01-06T03:44:57.029Z"}, {"id":4,"title":"Line Monitor Announcement","body":"This is a line monitor announcement. ","created_at":"2020-01-06T03:44:57.029Z","updated_at":"2020-01-06T03:44:57.029Z"}]
- 	}
- }];
-
- const ANNOUNCEMENTS = NOTIFICATIONS[0].data.announcements;
-
-class Notifications extends Component {
-  constructor(props) {
-    super(props);
-    this.props.getNotifications();
-  }
-
-  // item = ({ title }) => {
-  //   return (
-  //     <View style={styles.item}>
-  //       <Text style={styles.title}>{title}</Text>
-  //     </View>
-  //   );  
-  // }
-
-  renderSimpleListItem = (primary, secondary, time) => (
-    <ListItem style={styles.listItem}>
-      <Body>
-      <Text style={styles.time}>{moment(time).format('MMM Do YYYY, h:mm a')}</Text>
-        <Text style={styles.title}>{primary}</Text>
-        <Text style={styles.message}>{secondary}</Text>
-      </Body>
-    </ListItem>
-  )
-
-
+export default class Notifications extends Component {
   render() {
     return (
-      <View style={styles.notesContainer}>
-        <FlatList
-          // data={this.props.notifications.announcements}
-          data={ANNOUNCEMENTS}
-          renderItem={({ item }) => (
-            this.renderSimpleListItem(item.title, item.body, item.created_at)
-          )}
-      />
-      </View>
+      <Container>
+        <Content>
+          <Tabs>
+            <Tab heading="Line Monitor">
+              <UserAnnouncements />
+            </Tab>
+            <Tab heading="User">
+              <UserNotifications />
+            </Tab>
+          </Tabs>
+        </Content>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "500"
-  },
-  message: {
-    color: '#808080',
-  },
-  listItem: {
-    paddingVertical: 4,
-  },
-  time: {
-    fontWeight: '200',
-    fontSize: 12,
-    marginBottom: 5
-    
-    // textAlign:'right'
-  },
-});
 
 
-const mapStateToProps = state => ({
-  user: state.user,
-  notifications: state.notifications,
-});
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      getNotifications,
-    },
-    dispatch,
-  );
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Notifications);
