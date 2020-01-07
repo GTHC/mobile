1a5002bf1fce2d10e7e0efed47a3a77358e6b92d
Ansh Nanda <anshnanda10@gmail.com>
2020-01-07 07:54:20 -0500

    createSimpleList

--- app/containers/Notifications.js
+++ app/containers/Notifications.js
@@ -12,6 +12,15 @@ import { ScrollView } from 'react-native-gesture-handler';
 import { Title } from 'native-base';
 import { getNotifications} from '../redux/actions/notifications';
 
+const NOTIFICATIONS = [{"status":"SUCCESS",
+ "message":"Notifications for user with netid ssh50 found.",
+ "data":{
+ 	"notifications": [
+ 		{"id":75,"notification_id":"693b18a6-0703-4511-8bde-798f14db792a","start_time":"2020-01-09T09:20:00.000Z","title":"Upcoming Shift Reminder","content":"Hey! Your tent shift in K-Ville starts in 10 minutes!","user_id":31,"created_at":"2020-01-06T00:45:09.549Z","updated_at":"2020-01-06T00:45:09.549Z"}, {"id":75,"notification_id":"693b18a6-0703-4511-8bde-798f14db792a","start_time":"2020-01-09T09:20:00.000Z","title":"Upcoming Shift Reminder","content":"Hey! Your tent shift in K-Ville starts in 10 minutes!","user_id":31,"created_at":"2020-01-06T00:45:09.549Z","updated_at":"2020-01-06T00:45:09.549Z"}, {"id":75,"notification_id":"693b18a6-0703-4511-8bde-798f14db792a","start_time":"2020-01-09T09:20:00.000Z","title":"Upcoming Shift Reminder","content":"Hey! Your tent shift in K-Ville starts in 10 minutes!","user_id":31,"created_at":"2020-01-06T00:45:09.549Z","updated_at":"2020-01-06T00:45:09.549Z"}],
+ 	"announcements": [
+ 		{"id":1,"title":"Line Monitor Announcement","body":"This is a line monitor announcement. ","created_at":"2020-01-06T03:44:57.029Z","updated_at":"2020-01-06T03:44:57.029Z"}, {"id":1,"title":"Line Monitor Announcement","body":"This is a line monitor announcement. ","created_at":"2020-01-06T03:44:57.029Z","updated_at":"2020-01-06T03:44:57.029Z"}, {"id":1,"title":"Line Monitor Announcement","body":"This is a line monitor announcement. ","created_at":"2020-01-06T03:44:57.029Z","updated_at":"2020-01-06T03:44:57.029Z"}, {"id":1,"title":"Line Monitor Announcement","body":"This is a line monitor announcement. ","created_at":"2020-01-06T03:44:57.029Z","updated_at":"2020-01-06T03:44:57.029Z"}]
+ 	}
+ }];
 
 class Notifications extends Component {
   constructor(props) {
@@ -24,8 +33,17 @@ class Notifications extends Component {
       <View style={styles.item}>
         <Text style={styles.title}>{title}</Text>
       </View>
-    );
+    );  
   }
+
+  renderSimpleListItem = (primary, secondary) => (
+    <ListItem style={styles.container}>
+      <Body>
+        <Text style={styles.title}>{primary}</Text>
+        <Text >{secondary}</Text>
+      </Body>
+    </ListItem>
+  )
 
 
   render() {
@@ -33,8 +51,8 @@ class Notifications extends Component {
       <View style={styles.notesContainer}>
         <FlatList
           data={this.props.notifications.announcements}
-          renderItem={({ item }) => <Text style={styles.item}>{item.body}</Text>  
-        }
+          renderItem={({ item }) => (this.renderSimpleListItem(item.title, item.body)
+          )}
       />
       </View>
     );
