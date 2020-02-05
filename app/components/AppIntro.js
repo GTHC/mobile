4fdcfc926725bc97b2c247e60cd9d0c8d2aff672
Ansh Nanda <anshnanda10@gmail.com>
2020-02-05 16:07:48 -0500

    scrollvie fix and push

--- app/components/AppIntro.js
+++ app/components/AppIntro.js
@@ -18,33 +18,29 @@ export default class AppIntro extends Component {
     return (
       <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView}>
-          <View style={styles.body}>
-            <View>
-              <Text style={styles.titleText}>
-              Please Read The Following Info
-              </Text>
-              <Text style={styles.subTitle}>Especially if this is your first time using GTHC</Text>
-              <Image source={AppIntroPic} resizeMode="contain" style={{ width: '100%', height: '50%' }} />
-              <Text style={styles.text}>
+          <Text style={styles.titleText}>
+              Please Read Info
+          </Text>
+          <Text style={styles.subTitle}>Especially if this is your first time using GTHC</Text>
+          <Image source={AppIntroPic} resizeMode="contain" style={{ width: '100%', height: '50%' }} />
+          <Text style={styles.text}>
 Some of the features such as filling out your availibility and schedule automation
  are only availible on the web version of GTHC.
-              </Text>
-              <Text style={styles.text}>
+          </Text>
+          <Text style={styles.text}>
               The purpose of this mobile app
         is for shift reminder notifications, line monitor announcements
         , and a read-only view of all your teams shifts.
-              </Text>
-              <Text style={styles.text}>
+          </Text>
+          <Text style={styles.text}>
               Please logon to the web version at https://gthc.io/ to fully utilize all the features built into GTHC.
-              </Text>
-              <TouchableHighlight
-                onPress={this.exit}
-                style={styles.button}
-              >
-                <Text style={styles.buttonText}>Continue</Text>
-              </TouchableHighlight>
-            </View>
-          </View>
+          </Text>
+          <TouchableHighlight
+            onPress={this.exit}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Continue</Text>
+          </TouchableHighlight>
         </ScrollView>
       </SafeAreaView>
     );
@@ -57,8 +53,12 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight,
   },
   scrollView: {
-    backgroundColor: 'pink',
     marginHorizontal: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 15,
+    paddingBottom: 10,
+    flex: 1,
   },
   titleText: {
     fontSize: 24,
@@ -72,10 +72,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   body: {
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingTop: 15,
-    paddingBottom: 10,
   },
   button: {
     alignItems: 'center',
